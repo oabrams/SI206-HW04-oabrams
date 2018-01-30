a6df742baf01fd9079609c2f50a5316dad4ff86a
Orly Abrams <oabrams@umich.edu>
2018-01-30 18:34:15 -0500

    checkquestion

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,7 @@
+
 def question:
     x=input("What is your question?")
+    while x != "quit":
+        x=input("What is your question?")
+        if x[-1]!="?":
+            return "I'm sorry, I can only answer questions."
