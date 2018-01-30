c01fd867e05606d11029057d2865d408ad8ca632
Sofia Lotsoff <sofialo@umich.edu>
2018-01-30 18:54:37 -0500

    adding new code

---
+++ magic_eight.py
@@ -0,0 +1,10 @@
+import random
+
+def question:
+    x=input("What is your question?")
+
+    answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes"]
+
+    random_answer = random.choice(answers)
+    return random_answer
