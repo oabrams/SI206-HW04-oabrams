5d80db1bb3db31bdfae89f00f665b52dd46f1840
Orly Abrams <oabrams@umich.edu>
2018-02-02 16:33:45 -0500

    updating code

--- magic_eight.py
+++ magic_eight.py
@@ -1,21 +1,18 @@
-
-def question:
-    x=input("What is your question?")
-
-
 import random
 
-def question:
+def question():
     x=input("What is your question?")
 
     answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
     "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
     "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good" "Very doubtful"]
 
-    random_answer = random.choice(answers)
-    return random_answer
-
     while x != "quit":
-        x=input("What is your question?")
         if x[-1]!="?":
-            return "I'm sorry, I can only answer questions."
+            print( "I'm sorry, I can only answer questions.")
+        else:
+            random_answer = random.choice(answers)
+            print(random_answer)
+        x=input("What is your question?")
+
+question()
