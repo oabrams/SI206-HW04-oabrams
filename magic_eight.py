6c338e6006886905d3161c2be0f6d719d9b4b2f3
Max Lederman <ledmax@umich.edu>
2018-02-02 16:04:35 -0500

    trying to fix branch issues

---
+++ magic_eight.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+def question:
+    x=input("What is your question?")
+=======
+import random
+
+def question:
+    x=input("What is your question?")
+
+    answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes""Reply hazy try again", "Ask again later", "Better not tell you now",
+    "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good" "Very doubtful"]
+
+    random_answer = random.choice(answers)
+    return random_answer
+>>>>>>> add_questions2
