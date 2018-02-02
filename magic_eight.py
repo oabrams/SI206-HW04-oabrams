b665854d9e813db393f06f4bdf12452daba2ab90
Max Lederman <ledmax@umich.edu>
2018-02-02 16:06:57 -0500

    added comma

--- magic_eight.py
+++ magic_eight.py
@@ -8,7 +8,7 @@ def question:
     x=input("What is your question?")
 
     answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
-    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes""Reply hazy try again", "Ask again later", "Better not tell you now",
+    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now",
     "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good" "Very doubtful"]
 
     random_answer = random.choice(answers)
